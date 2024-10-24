84a49b95597a8b9c64a09877e39754e0d006c36c
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-10-24 09:14:28 +0200

    Base project

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SpmWithRealm",
+    products: [
+        .library(
+            name: "SpmWithRealm",
+            targets: ["SpmWithRealm"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/realm-swift", .upToNextMajor(from: "10.46.0"))
+    ],
+    targets: [
+        .target(
+            name: "SpmWithRealm",
+            dependencies: [
+                .product(name: "Realm", package: "realm-swift"),
+                .product(name: "RealmSwift", package: "realm-swift")
+            ]
+        ),
+        .testTarget(
+            name: "SpmWithRealmTests",
+            dependencies: ["SpmWithRealm"]
+        ),
+    ]
+)
